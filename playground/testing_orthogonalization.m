86742323a13e3406cb4f8be9a4ff1b158758b32c
Matteo Demuru <suforraxi@gmail.com>
2020-02-12 11:32:16 +0100

    adjusting the plots

--- playground/testing_orthogonalization.m
+++ playground/testing_orthogonalization.m
@@ -17,16 +17,31 @@ for i = 1 : 2
 end
 y(end,:) = x(end,:);
 
-% plot in different lines
-figure
+% orthogonalization
+om = get_ortho_matrix(y,3);
+
+% plot signals in different lines and correlation matrices
+% signals
+f(1) = figure;
+ay_lim = [-4 25];
+subplot(1,3,1)
 xx = x + repmat([0, 10, 20]',1,size(x,2));
 plot(xx')
-
-figure
+title('Original')
+ylim(ay_lim)
+subplot(1,3,2)
 yy = y + repmat([0, 10, 20]',1,size(y,2));
 plot(yy')
+title('Mixed')
+ylim(ay_lim)
 
+omm = om + repmat([0, 10]',1,size(om,2));
+subplot(1,3,3)
+plot(omm')
+title('Orthogonalized')
+ylim(ay_lim)
 
+% correlation matrices
 figure;
 imLim = [-1 1];
 
@@ -44,13 +59,11 @@ imagesc(mixed(1:2,1:2),imLim)
 title('Naive')
 
 subplot(2,2,4)
-om = get_ortho_matrix(y,3);
+
 imagesc(corr(om'),imLim)
 title('Orthogonalized')
 
 colorbar('manual','Position',[0.93,0.1,0.03,0.8])
 
-figure;
-omm = om + repmat([0, 10]',1,size(om,2));
-plot(omm')
+
 
