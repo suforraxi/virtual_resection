97892708db1c54bf47c45e2a244d7fb7d91e2139
Matteo Demuru <suforraxi@gmail.com>
2020-04-08 17:27:21 +0200

    added gnu copyright inside the files

--- analysis_util/find_artefacts.m
+++ analysis_util/find_artefacts.m
@@ -17,6 +17,22 @@
 %                 number of trials. Every entry contains 1 if the
 %                 corresponding trial contains at least one channel
 %                 with an artefact, 0 no artefact   
+
+%     Copyright (C) 2020 Matteo Demuru
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 function [idxChArtefact ,idx_art_trial] = find_artefacts(s,labels,artefact_T)
 
 artefact_T;
--- analysis_util/get_resected_label.m
+++ analysis_util/get_resected_label.m
@@ -1,3 +1,33 @@
+% Get resected channels for bipolar montage
+% 
+% INPUT
+%      chName      - cell with the label for bipolar derivation (channels)
+%      res_channel - cell with the labels with resected channels as
+%                    recorded (common reference)
+% OUTPUT
+%
+%     res_notR_cut - string indicating if the bipolar channel was
+%                    resected (RES) both monopolar channels resected 
+%                    not resected (NRES) both monopolar channels not resected  
+%                    cut (CUT) one monopolar channel resected the other not
+%                    resected
+%
+%
+
+%     Copyright (C) 2020 Matteo Demuru
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function res_notR_cut = get_resected_label(chName,res_channel)
 
